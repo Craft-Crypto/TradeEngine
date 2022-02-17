ca91e0e690c994cb52aab3658d194b0199b709b7
tommc <brewers@craft-crypto.com>
2022-02-17 09:50:22 -0500

    Adding License Details

--- to_deploy/main.py
+++ to_deploy/main.py
@@ -1,3 +1,36 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2022 Tom McLaughlin @ Craft-Crypto, LLC
+#
+# Other libraries used in the project:
+#
+# Copyright (c) 2015-2019 Digital Sapphire - PyUpdater
+# Copyright (c) 2017 Igor Kroitor - ccxt
+# Copyright (c) 2018 P G Jones - hypercorn
+# Copyright (c) 2017 P G Jones - quart
+# Copyright (c) 2007 vxgmichel - aioconsole
+# Copyright (c) 2013-2021 Aymeric Augustin and contributors - websockets
+# Copyright (c) 2017-2018 Alex Root Junior - aiogram
+# Copyright (c) 2022 Craft-Crypto, LLC - Craft-Crypto Helpers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# -----------------------------------------------------------------------------
+
 import CraftCrypto_Helpers.Helpers
 import pyupdater
 from pyupdater.client import Client as updateClient
@@ -21,7 +54,7 @@ def check_for_update(te_name, te_version):
         ClientConfig(), refresh=True, progress_hooks=[print_status_info]
     )
     my_update = up_client.update_check(te_name, te_version)
-    print(my_update)
+    # print(my_update)
     if isinstance(my_update, pyupdater.client.updates.LibUpdate):
         print('Library Found Update.')
     elif isinstance(my_update, pyupdater.client.updates.AppUpdate):
@@ -62,7 +95,7 @@ if __name__ == "__main__":
                     time.sleep(.25)
                 my_update.extract()
                 dir = my_update.update_folder
-                print(my_update.update_folder, my_update.filename)
+                # print(my_update.update_folder, my_update.filename)
                 # opening the zip file in read mode.
                 with zipfile.ZipFile(my_update.update_folder + '/' + my_update.filename, "r") as zf:
                     zf.extractall(my_update.update_folder)
