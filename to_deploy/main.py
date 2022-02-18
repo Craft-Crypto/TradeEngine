cddcffb12eb7bbfece0a4b153b7dc250bfa03808
tommc <brewers@craft-crypto.com>
2022-02-18 00:40:48 -0500

    inc version

--- to_deploy/main.py
+++ to_deploy/main.py
@@ -77,7 +77,7 @@ def print_status_info(info):
 
 if __name__ == "__main__":
     te_name = 'TradeEngine'
-    te_version = '1.0.4'
+    te_version = '1.0.5'
 
     try:
         print('Welcome to TradeCraft Lite v' + te_version)
